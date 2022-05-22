45d0e00d2815f66200c7b9e2a4163a883991bceb
hapily04 <ryanbauwens1@gmail.com>
2022-05-21 23:59:12 -0500

    Initial Commit

--- src/main/java/me/hapily/plugins/util/paperpluginutil/PaperPluginUtil.java
+++ src/main/java/me/hapily/plugins/util/paperpluginutil/PaperPluginUtil.java
@@ -1,13 +1,14 @@
 package me.hapily.plugins.util.paperpluginutil;
 
 import me.hapily.plugins.util.paperpluginutil.menus.Menu;
+import me.hapily.plugins.util.paperpluginutil.menus.MenuManager;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaperPluginUtil {
 
     public PaperPluginUtil(JavaPlugin plugin){
-        // register all of the
+        new MenuManager(plugin);
     }
 
 }
